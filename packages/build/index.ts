3946f499b087eb1fb3e5c66eef8119ea9c293a71
John Royal <34844819+john-royal@users.noreply.github.com>
2025-05-14 17:26:30 -0400

    fix(build): troubleshoot railway

--- packages/build/index.ts
+++ packages/build/index.ts
@@ -1,5 +1,7 @@
-export default {
-  fetch: async (request: Request) => {
+Bun.serve({
+  hostname: "::",
+  port: process.env.PORT ?? 3000,
+  fetch: async (request) => {
     return new Response("Hello, world!");
   },
-};
+});
