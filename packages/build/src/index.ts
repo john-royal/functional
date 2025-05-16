1fee32489d9b4fb0d9f0c259140f9803edf5343b
John Royal <34844819+john-royal@users.noreply.github.com>
2025-05-16 13:52:45 -0400

    chore(build): remove unused deps

--- packages/build/src/index.ts
+++ packages/build/src/index.ts
@@ -1,9 +1,1 @@
-import { db, env } from "./lib";
-
-export default {
-  port: env.PORT,
-  fetch: async (request: Request) => {
-    const data = await db.query.teams.findMany();
-    return Response.json(data);
-  },
-} satisfies Bun.Serve;
+import { env } from "./lib";
--- packages/build/src/lib.ts
+++ packages/build/src/lib.ts
@@ -1,21 +1,9 @@
-import { createDatabaseClient } from "@functional/db/client";
-import { createEnv } from "@t3-oss/env-core";
-import { App } from "octokit";
 import { z } from "zod";
 
-export const env = createEnv({
-  server: {
-    PORT: z.number().default(3000),
-    DATABASE_URL: z.string(),
-    GITHUB_APP_ID: z.string(),
-    GITHUB_PRIVATE_KEY: z.string(),
-  },
-  runtimeEnv: process.env,
+const EnvSchema = z.object({
+  API_URL: z.string(),
+  INPUT_TOKEN: z.string(),
+  OUTPUT_TOKEN: z.string(),
 });
 
-export const db = createDatabaseClient(env.DATABASE_URL);
-
-export const app = new App({
-  appId: env.GITHUB_APP_ID,
-  privateKey: env.GITHUB_PRIVATE_KEY,
-});
+export const env = EnvSchema.parse(process.env);
